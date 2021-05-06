2cea8ce874a30c28a69c4f96c793db9e74810c83
Josh <45292743+DoublePly@users.noreply.github.com>
2021-05-06 19:24:02 +0100

    Update senseHat_MemoryGame.py
    First version of the game is complete. This version allows a player to select from two practice game modes and tracks their scores.

--- senseHat_MemoryGame.py
+++ senseHat_MemoryGame.py
@@ -6,19 +6,19 @@ import random
 sense = SenseHat()
 sense.clear()
 
-#Variables for display colours
+#Variables for RGB values for LED display
 
-a = (255,10,10)
-b = (10,10,255)
+a = (0,0,0)
+b = (255,255,255)
 r = (255,0,0)
-u = (0,0,0)
 g = (0,255,0)
 
-
+#Variables for the game. The two "extra" lists are used to populate the remaining spots
+#of the options from which the player will guess the right choice.
 
 char_meaning = None
 char_pronounce = None
-
+file_contents = []
 extra_meaning = ['sugar','milk','tea','coffee','water','I','love','one','two','three','four','five']
 extra_pronunciation = ['ng5','sei3','ji6','jat1','siu2','do1','luk6','tong4','bui1','ngo5','oi3']
 game_meaning_list = []
@@ -26,138 +26,167 @@ game_pronunciation_list =[]
 points_counter = 0
 row_counter = 0
 max_rows = 0
-points_aim = 0
 
-# The following function loads a character, its meaning, and pronunciation from the CSV file.
 
-def load_character(row_number):
+#read_data will load the data needed to render the characters and
+#pass their correct meaning/pronunciation to the right variables. It also determines
+#how many rows are in the file to ensure that the game does not try to request more data than is there.
+
+def read_data():
+
+    global max_rows
+    global file_contents
+    try:
+        with open('cantonese_characters.txt') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            file_contents = list(csv_reader)
+            max_rows = len(file_contents)
+            
+    except:
+        sense.show_message("Issue reading from file. Make sure the file exists then restart the game.")
+                
+  
+#load_character takes in two values: row_counter (which keeps track of the row the game is on), and file_contents
+#(which is the contents of the read file saved to a list). row_counter is used as the index value to obtain the right row from
+#the list. It then pops off the last two values of the list which should be the meaning/pronunciation. Then it converts the
+#remaining String values to variable names (otherwise it won't display properly)
+
+def load_character(row_counter, file_contents):
     
     global char_meaning
     global char_pronounce
-    global max_rows
-    with open('cantonese_characters.txt') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        rows = list(csv_reader)
-        max_rows = len(rows)
-        print(rows)
-        hold_list = rows[row_number]
-        temp_display_char = hold_list
-        print(temp_display_char)
+    try:
+        file_row = file_contents[row_counter]
         display_char = []
-        char_pronounce = temp_display_char.pop(65)
-        char_meaning = temp_display_char.pop(64)
-        for item in temp_display_char:
+        char_pronounce = file_row.pop(65)
+        char_meaning = file_row.pop(64)
+        for item in file_row:
             if item == 'a':
                 display_char.append(a)
             if item =='b':
                 display_char.append(b)
-        #char_meaning = hold_list[64]
-        #char_pronounce = hold_list[65]
-        print (display_char)
-        print (len(display_char))
         sense.set_pixels(display_char)
         time.sleep(3)
         sense.clear()
-        print (char_meaning)
+        
+    except:
+        sense.show_message("Check the contents of the supporting file. Data formatting may not be correct.")
 
-# The function that sets up the list of meanings to be used by the game.
+#create_meaning_list generates a list of 4 elements. It initially populates it with four incorrect answers
+#using extra_meaningand then overrides an element at a random index with the correct answer.
 
 def create_meaning_list():
     global game_meaning_list
-    #Populates the list with four values that aren't the answer
+    global char_meaning
+    game_meaning_list = []
+    
     while len(game_meaning_list) < 4:
         holding_variable = random.choice(extra_meaning)
-        if holding_variable != char_meaning:
+        if holding_variable != char_meaning and holding_variable not in game_meaning_list:
             game_meaning_list.append(holding_variable)
-    #Changes one element at random to the answer
+   
     game_meaning_list[random.randrange(4)] = char_meaning 
-    print (game_meaning_list)
+   
     
-# The function that sets up the list of pronunciations to be used by the game.
+#create_pronunciation_list generates a list of 4 elements. It initially populates it with four incorrect answers
+#using extra_pronunciation and then overrides an element at a random index with the correct answer.
     
 def create_pronunciation_list():
     global game_pronunciation_list
+    global char_pronounce
     
-    #Populates the list with four values that aren't the answer
-    
+    game_pronunciation_list = []
     while len(game_pronunciation_list) < 4:
         holding_variable = random.choice(extra_pronunciation)
-        if holding_variable != char_meaning:
+        if holding_variable != char_pronounce and holding_variable not in game_pronunciation_list:
             game_pronunciation_list.append(holding_variable)
-            
-    #Changes one element at random to the answer
+
             
     game_pronunciation_list[random.randrange(4)] = char_pronounce 
-    print (game_pronunciation_list)
+    
+
+#pronunciation_game runs the pronunciation game mode where the player attempts to guess the correct
+#pronunciations of loaded characters. After each attempt it will increment the row_counter to render
+#the next character. Due to max_rows the game will stop when each character has been tested. 
     
 def pronunciation_game():
     global points_counter
     global row_counter
-    
-    
-    for x in range(0, 5):
-        load_character(row_counter)
+    read_data()
+    for x in range(0, max_rows):
+        load_character(row_counter, file_contents)
         create_pronunciation_list()
         for i in game_pronunciation_list:
             sense.show_message(i)
         sense.show_message("Pick!", scroll_speed=0.05)
         event_occured = False
         while event_occured == False:
-            
-            for event in sense.stick.get_events():
+           for event in sense.stick.get_events():
                 if event.action == "pressed":
                     if pronunciation_outcome(event.direction) == True:
-                        sense.show_message("Right", text_colour=(g), back_colour=(u), scroll_speed=0.05)
-                        points_counter ++ 1
-                        row_counter ++ 1
+                        sense.show_message("Right", text_colour=(g), back_colour=(a), scroll_speed=0.05)
+                        points_counter = points_counter + 1
+                        row_counter = row_counter + 1
                         event_occured = True
                     else:
-                        sense.show_message("Wrong", text_colour=(r), back_colour=(u), scroll_speed=0.05)
-                        row_counter = ++ 1
+                        sense.show_message("Wrong", text_colour=(r), back_colour=(a), scroll_speed=0.05)
+                        row_counter = row_counter + 1
                         event_occured = True
-
+                        
+                        
+#meaning_game runs the pronunciation game mode where the player attempts to guess the correct
+#meanings of loaded characters. After each attempt it will increment the row_counter to render
+#the next character. Due to max_rows the game will stop when each character has been tested.
+                        
 def meaning_game():
     global points_counter
-    minigame_over = False
-    while minigame_over == False:
-        load_character()
+    global row_counter
+    read_data()
+    for x in range(0, max_rows):
+        load_character(row_counter, file_contents)
         create_meaning_list()
         for i in game_meaning_list:
             sense.show_message(i)
         sense.show_message("Pick!", scroll_speed=0.05)
-        event = sense.stick.wait_for_event()
-        for event in sense.stick.get_events():
-            if event.action == "pressed":
-                
-                if meaning_outcome(event.direction) == True:
-                    sense.show_message("Right",text_colour=(g), back_colour=(u), scroll_speed=0.05)
-                    minigame_over = True
-                else:
-                    sense.show_message("Wrong", text_colour=(r), back_colour=(u), scroll_speed=0.05)
+        event_occured = False
+        while event_occured == False:
+            for event in sense.stick.get_events():
+                if event.action == "pressed":
+                    if meaning_outcome(event.direction) == True:
+                        sense.show_message("Right", text_colour=(g), back_colour=(a), scroll_speed=0.05)
+                        points_counter = points_counter + 1
+                        row_counter = row_counter + 1
+                        event_occured = True
+                    else:
+                        sense.show_message("Wrong", text_colour=(r), back_colour=(a), scroll_speed=0.05)
+                        row_counter = row_counter + 1
+                        event_occured = True
+                        
+                        
+#pronunciation_outcome takes a String value representing a direction and then uses that to verify that it
+#represented the correct answer. It does this by calling a further function input_Parser.
 
 def pronunciation_outcome(direction):
     
-    #Get the index of the correct answer.
-    
     index = game_pronunciation_list.index(char_pronounce)
-    print(index)
     if index == input_Parser(direction):
         return True
     else:
         return False
     
-
+#meaning_outcome takes a String value representing a direction and then uses that to verify that it
+#represented the correct answer. It does this by calling a further function input_Parser.
+    
 def meaning_outcome(direction):
     
-    #Get the index of the correct answer.
-    
     index = game_meaning_list.index(char_meaning)
-    
     if index == input_Parser(direction):
         return True
     else:
         return False
  
+#input_Parser converts a String value representing direction into a numerical value and returns it. That value represents
+#an index value of a list. 
     
 def input_Parser(input):
     if input == "left":
@@ -172,31 +201,27 @@ def input_Parser(input):
     elif input == "down":
         return 3
         
-        
-def game_rules():
-    sense.show_message("Cantonese Memory Game! To select answers use the joystick: Left,Up, Down, Right correspond to the order in which the choices are shown.Now pick a mode: Left for Pronunciation. Right for Meaning.", scroll_speed=0.05)
 
 
+
+    
+
+#Sets up a Boolean value that will keep the game running until the player has played one of the game modes.Then starts the game.
+    
 game_over = False
-game_rules()
+sense.show_message("Cantonese Memory Game! Now pick a mode: Left for Pronunciation. Right for Meaning.", scroll_speed=0.05)
 
 while game_over == False:
-    points_counter
     for event in sense.stick.get_events():
-        
         if event.action == "pressed":
-            
-            #Check Direction
-            
             if event.direction == "left":
                 sense.show_message("Pronunciation practice!", scroll_speed=0.05)
                 pronunciation_game()
+                sense.show_message("Your score: " + str(points_counter))
+                game_over = True
             if event.direction == "right":
                 sense.show_message("Meaning practice!", scroll_speed=0.05)
                 meaning_game()
-                
-            
+                sense.show_message("Your score: " + str(points_counter))
+                game_over = True
             sense.clear()
-        
-
-
