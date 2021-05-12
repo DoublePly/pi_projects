898a1a8ec8f59bb5ea32be50228e20eabe9d577e
Josh <45292743+DoublePly@users.noreply.github.com>
2021-05-12 18:54:51 +0100

    Added comments

--- senseHat_TempDisplay.py
+++ senseHat_TempDisplay.py
@@ -4,15 +4,19 @@ import time
 sense = SenseHat()
 sense.clear()
 
+# Sets up the variables used to display colours to the LED Matrix. Each variable is a tuple representing an RGB value. So Black, Red, Orange, and Yellow in 
+# that order.
 
 b = (0,0,0)
 r = (255,0,0)
 o = (255,153,51)
 y = (255,255,51)
 
-
+# Sets up the variable that will be used to store the temperature readings. 
 
 currentTemp = 0
+
+# Sets up the first display on the LED Matrix. Which in this case is a full Black background.
 
 display = [ b,b,b,b,b,b,b,b,
             b,b,b,b,b,b,b,b,
@@ -23,6 +27,9 @@ display = [ b,b,b,b,b,b,b,b,
             b,b,b,b,b,b,b,b,
             b,b,b,b,b,b,b,b,
     ]
+# These variables are lists of values that represent the indexes of each position in a typical 64 element list used to set the 8x8 LED Matrix. They've been
+# ordered into columns from left to right so as to correctly alter the display and adjust it as the temperature reading gets updated.
+
 columnOne =   [0,8,16,24,32,40,48,56]
 columnTwo =   [1,9,17,25,33,41,49,57]
 columnThree = [2,10,18,26,34,42,50,58]
@@ -32,14 +39,20 @@ columnSix =   [5,13,21,29,37,45,53,61]
 columnSeven = [6,14,22,30,38,46,54,62]
 columnEight = [7,15,23,31,39,47,55,63]
  
+            
+# Sets up the initial display for the LED Matrix.
+            
 sense.set_pixels(display)
 
+# A function to obtain the current temperature and convert the returned float value into an int value.
 
 def getTemp():
     global currentTemp
     currentTemp = int(sense.get_temperature_from_humidity())
 
-
+# A function made to alter the display. Basically it uses the values stored in the column variables to iterate over the display
+# list column by column, moving each current colour value over to the left by one (to give the appearance of a constant running line
+# on the LED display. The first column is wiped completely as they've hit the edge of the screen.)
     
 def alterDisplay():
     global display
@@ -93,7 +106,8 @@ def alterDisplay():
             
     display = displayCopy
     
-
+# This iterates continuously, calling the two other functions. If the temperature is in a certain range it will add that colour to a specific position
+# in the last column of the LED Matrix. After which it pauses very briefly before doing it all over again.
 
 while True:
     time.sleep(0.2)
